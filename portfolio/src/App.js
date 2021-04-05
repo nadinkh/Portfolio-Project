a9d9fdeb94acfdcdbfdc2613ae09f26ec0a1999c
nadin <nadin_khamis@github.com>
2021-04-05 13:50:47 +0300

    first commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,41 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import Particles from "react-particles-js"
 function App() {
   return (
-    <div>
-      <Navbar />
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              },
+
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            },
+
+          }
+        }}
 
 
-    </div>
+      />
+      <div>
+        <Navbar />
+        <Header />
+      </div>
+    </>
   );
 }
 
